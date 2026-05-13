90de2e4559a1641b11cf1cc2d53b1754ce2a9d23
AKL001 <l9udyr@gmail.com>
2026-05-13 19:07:17 +0100

    wordpress fastcgi process manager configuration with nginx

---
+++ srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# if any error happens we exit
+set -e
+
+#############################################################################
+# we already handle the health check for mariadb in the docker compose file##
+#############################################################################
+
+
